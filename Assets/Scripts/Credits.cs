b5620e5d3695652651f6e483eec77f7cdde93fee
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-12-01 15:12:15 -0600

    I hate untiy

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour {
+
+    //THIS FILE IS FOR GIVING CREDIT TO FREE ASSETS
+    //MAIN MENU LOGO CREATED AT COOLTEXT.COM
+    //STOCK SOUND EFFECTS FROM SOUNDBIBLE
+    //STOCK MUSIC FROM INCOMPETECH.COM
+    //ALL STOCK ITEMS USED BELONG TO THE ORIGINAL CREATORS
+    
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
